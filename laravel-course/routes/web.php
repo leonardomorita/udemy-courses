c7d68b1fb7b21f105ca4eb87b1f3b5e3358e8963
leonardomorita <leonardomorita2@gmail.com>
2020-07-09 23:20:45 -0300

    Add route's authentication and add navbar.

--- laravel-course/routes/web.php
+++ laravel-course/routes/web.php
@@ -13,22 +13,25 @@
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
-Route::prefix('admin')->name('admin.')->namespace('Admin')->group(function () {
-    // Route::prefix('stores')->name('stores.')->group(function () {
-    //     // Route::get('/stores', 'Admin\\StoreController@index');
-    //     Route::get('/', 'StoreController@index')->name('index');
-    //     Route::get('/create', 'StoreController@create')->name('create');
-    //     Route::post('/store', 'StoreController@store')->name('store');
-    //     Route::get('/{store}/edit', 'StoreController@edit')->name('edit');
-    //     Route::post('/update/{store}', 'StoreController@update')->name('update');
-    //     Route::get('/destroy/{store}', 'StoreController@destroy')->name('destroy');
-    // });
-    Route::resource('stores', 'StoreController');
-    Route::resource('products', 'ProductController');
+// Adicionando para as rotas que estão dentro do grupo, um middleware de verificação de autenticação do usuário
+Route::group(['middleware' => ['auth']], function () {
+    Route::prefix('admin')->name('admin.')->namespace('Admin')->group(function () {
+        // Route::prefix('stores')->name('stores.')->group(function () {
+        //     // Route::get('/stores', 'Admin\\StoreController@index');
+        //     Route::get('/', 'StoreController@index')->name('index');
+        //     Route::get('/create', 'StoreController@create')->name('create');
+        //     Route::post('/store', 'StoreController@store')->name('store');
+        //     Route::get('/{store}/edit', 'StoreController@edit')->name('edit');
+        //     Route::post('/update/{store}', 'StoreController@update')->name('update');
+        //     Route::get('/destroy/{store}', 'StoreController@destroy')->name('destroy');
+        // });
+        Route::resource('stores', 'StoreController');
+        Route::resource('products', 'ProductController');
+    });
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
