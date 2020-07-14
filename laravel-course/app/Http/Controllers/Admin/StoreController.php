307b982fcdfaa4b48ae400f5d3baa61cbbda78f9
leonardomorita <leonardomorita2@gmail.com>
2020-07-14 12:07:22 -0300

    Cria o StoreRequest para verificação dos campos do formulário da criação de uma nova loja.

--- laravel-course/app/Http/Controllers/Admin/StoreController.php
+++ laravel-course/app/Http/Controllers/Admin/StoreController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreRequest;
 
 class StoreController extends Controller
 {
@@ -21,7 +21,7 @@ class StoreController extends Controller
         return view('admin.store.create', compact('users'));
     }
 
-    public function store(Request $request)
+    public function store(StoreRequest $request)
     {
         $data = $request->all();
 
@@ -41,7 +41,7 @@ class StoreController extends Controller
         return view('admin.store.edit', compact('store'));
     }
 
-    public function update(Request $request, $store)
+    public function update(StoreRequest $request, $store)
     {
         $data = $request->all();
 
