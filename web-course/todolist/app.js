f7506a311ce3530f582d15c0af9cf073a9033d08
leonardomorita <leonardomorita2@gmail.com>
2020-07-27 11:49:57 -0300

    Adiciona a persistência dos dados dentro do banco de dados

--- web-course/todolist/app.js
+++ web-course/todolist/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const date = require(__dirname + '/date.js');
+const mongoose = require('mongoose');
+
+// const date = require(__dirname + '/date.js');
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -8,34 +10,70 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const activities = [];
-const works = [];
+mongoose.connect("mongodb://localhost:27017/todolist", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const activitySchema = new mongoose.Schema ({
+    name: {
+        type: String,
+        required: true
+    }
+});
+
+const Activity = mongoose.model('Activity', activitySchema);
 
 app.get('/', function (req, res) {
-    const day = date.getDate();
+    // const day = date.getDate();
 
-    res.render('list', {listTitle: day, activities: activities});
+    Activity.find({}, {"name": true}, function (err, activities) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (activities.length === 0) {
+                const playSports = new Activity ({
+                    name: "Play sports"
+                });
+
+                const read = new Activity ({
+                    name: "Read"
+                });
+
+                const study = new Activity ({
+                    name: "Study"
+                });
+
+                Activity.insertMany([playSports, read, study], function (err) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log("OK!");
+                    }
+                });
+            }
+
+            res.render('list', {listTitle: "Today", activities: activities});
+        }
+    });
 });
 
 app.post('/', function (req, res) {
     const activity = req.body.activity;
 
     if (req.body.button === "Work") {
-        works.push(activity);
+        
         res.redirect("/work");
     } else {
-        activities.push(activity);
+        
         res.redirect('/');
     }
 });
 
 app.get('/work', function (req, res) {
-    res.render("list", {listTitle: "Work", activities: works});
+    res.render("list", {listTitle: "Work"});
 });
 
 app.post('/work', function (req, res) {
     const item = req.body.activities;
-    works.push(item);
+    
 
     res.redirect("/work");
 });
