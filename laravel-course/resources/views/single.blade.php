17959c4951b4fc57e5877127b905202f8b46ffef
leonardomorita <leonardomorita2@gmail.com>
2020-09-04 10:33:52 -0300

    Close #14

--- laravel-course/resources/views/single.blade.php
+++ laravel-course/resources/views/single.blade.php
@@ -3,13 +3,24 @@
 @section('content')
     <div class="row">
         <div class="col-lg-4">
-
+            @if ( $product->images->count() )
+                <img class="img-fluid" src="{{ asset('storage/' . $product->images->first()->image) }}" alt="image-photo.png">
+                <div class="row mt-2">
+                    @foreach ($product->images as $image)
+                        <div class="col-4">
+                            <img class="img-fluid" src="{{ asset('storage/' . $image->image) }}" alt="image-photo.png">
+                        </div>
+                    @endforeach
+                </div>
+            @else
+                <img class="card-img-top" src="{{ asset('assets/images/no-photo.jpg') }}" alt="no-photo.jpg">
+            @endif
         </div>
 
         <div class="col-lg-8">
             <h2>{{ $product->name }}</h2>
             <p>{{ $product->description }}</p>
-            <h3>{{ $product->price }}</h3>
+            <h3>R$ {{ number_format($product->price, '2', ',', '.') }}</h3>
             <span>Loja: {{ $product->store->name }}</span>
         </div>
     </div>
