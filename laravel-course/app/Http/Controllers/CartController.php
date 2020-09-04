b8472ecdd687d15d172419931743f5443f816aa9
leonardomorita <leonardomorita2@gmail.com>
2020-09-04 11:33:19 -0300

    Close #16

--- laravel-course/app/Http/Controllers/CartController.php
+++ laravel-course/app/Http/Controllers/CartController.php
@@ -6,9 +6,26 @@ use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
+    public function index()
+    {
+        dd(session()->get('cart'));
+    }
+
     public function add(Request $request)
     {
         $product = $request->product;
-        dd($product);
+
+        // Verifica na sessão a existência da variável 'cart'
+        if (session()->has('cart')) {
+            // Existe a variável 'cart' na sessão, então vai ser adicionado um novo produto na variável 'cart' da sessão
+            session()->push('cart', $product);
+        } else {
+            // Não existe a variável 'cart' na sessão, então vai ser criado a mesma na sessão com o primeiro produto
+            $products[] = $product;
+            session()->put('cart', $products);
+        }
+
+        flash('O produto foi adicionado no carrinho.')->success();
+        redirect()->route('product.single', ['slug' => $product['slug']]);
     }
 }
